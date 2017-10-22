6f1c0db387ce7209ad1003a1d9de5491ef1bb86b
Mikus Miroslav <miroslav.mikus@outlook.com>
2017-10-22 13:59:24 +0200

    Add Book Controller and View

---
+++ Controllers/BookController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LibraryManagement.Data.Interfaces;
+using LibraryManagement.Data.Model;
+using LibraryManagement.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LibraryManagement.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookRepository _repository;
+        private readonly IAuthorRepository _authorRepository;
+
+        public BookController(IBookRepository repository, IAuthorRepository authorRepository)
+        {
+            _repository = repository;
+            _authorRepository = authorRepository;
+        }
+        [Route("Book")]
+        public IActionResult List(int? authorId, int? borrowerId)
+        {
+            IEnumerable<Book> books;
+
+            ViewBag.AuthorId = authorId;
+
+            if(borrowerId != null)
+            {
+                books = _repository.FindWithAuthorAndLender(x => x.BorrowerId == borrowerId);
+                return CheckBooksCount(books);
+            }
+
+            if (authorId == null)
+            {
+                books = _repository.GetAllWithAuthor();
+                return CheckBooksCount(books);
+            }
+            else
+            {
+                var author = _authorRepository.GetWithBooks((int)authorId);
+
+                if (author.Books == null || author.Books.Count == 0)
+                    return View("EmptyAuthor", author);
+            }
+
+            books = _repository.FindWithAuthor(a => a.Author.AuthorId == authorId);
+
+            return CheckBooksCount(books);
+        }
+
+        private IActionResult CheckBooksCount(IEnumerable<Book> books)
+        {
+            if (books == null || books.ToList().Count == 0)
+            {
+                return View("Empty");
+            }
+            else
+            {
+                return View(books);
+            }
+        }
+
+        public IActionResult Update(int id)
+        {
+            Book book = _repository.FindWithAuthor(a => a.BookId == id).FirstOrDefault();
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            var bookVM = new BookEditViewModel
+            {
+                Book = book,
+                Authors = _authorRepository.GetAll()
+            };
+
+            return View(bookVM);
+        }
+
+        [HttpPost]
+        public IActionResult Update(BookEditViewModel bookVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                bookVM.Authors = _authorRepository.GetAll();
+                return View(bookVM);
+            }
+            _repository.Update(bookVM.Book);
+
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Create()
+        {
+            var bookVM = new BookEditViewModel
+            {
+                Authors = _authorRepository.GetAll()
+            };
+
+            return View(bookVM);
+        }
+
+        [HttpPost]
+        public IActionResult Create(BookEditViewModel bookVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                bookVM.Authors = _authorRepository.GetAll();
+                return View(bookVM);
+            }
+
+            _repository.Create(bookVM.Book);
+
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Delete(int id, int? authorId)
+        {
+            var book = _repository.GetById(id);
+
+            _repository.Delete(book);
+
+            return RedirectToAction("List", new { authorId = authorId });
+        }
+    }
+}
---
+++ ViewModels/BookEditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using LibraryManagement.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.ViewModels
+{
+    public class BookEditViewModel
+    {
+        public Book Book { get; set; }
+
+        public IEnumerable<Author> Authors { get; set; }
+    }
+}
