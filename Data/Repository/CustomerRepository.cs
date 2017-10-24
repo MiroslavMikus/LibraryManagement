000513a38086e965592e1d2f85a476d64d3b54e8
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2017-10-24 03:46:24 +0000

    Deleted customer will give bask all his borrowed books before he is deleted

--- Data/Repository/CustomerRepository.cs
+++ Data/Repository/CustomerRepository.cs
@@ -10,5 +10,18 @@ namespace LibraryManagement.Data.Repository
     public class CustomerRepository : Repository<Customer>, ICustomerRepository
     {
         public CustomerRepository(LibraryDbContext context) : base(context) { }
+
+        public override void Delete(Customer entity)
+        {
+            _context.Books.Where(b => b.Borrower == entity)
+                .ToList()
+                .ForEach(a =>
+                  {
+                      a.Borrower = null;
+                      a.BorrowerId = 0;
+                  });
+
+            base.Delete(entity);
+        }
     }
 }
